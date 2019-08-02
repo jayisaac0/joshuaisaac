5c20d79ed788b69c72c42fc05f16096a64c4d84d
Joshua Isaac <jayisaac0@gmail.com>
2019-08-02 09:34:37 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,202 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Specific Meta
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="Personal Portfolio is a modern presentation HTML5 Portfolio HTML Template.">
+    <meta name="keywords" content="HTML5, Template, Design, portfolio, Personal Portfolio, Single portfolio" />
+    <meta name="author" content="HTMLguru">
+
+    <!-- Titles
+    ================================================== -->
+    <title>Personal Portfolio | 404 Error Dark</title>
+
+    <!-- Favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="assets/images/site-icon.png">
+    <link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/images/apple-touch-icon-114x114.png">
+
+    <!-- Custom Font
+    ================================================== -->
+    <link href="https://fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700,800" rel="stylesheet">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/simple-scrollbar.css">
+    <link rel="stylesheet" href="assets/css/odometer-theme-default.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <script src="assets/js/modernizr.min.js"></script>
+</head>
+
+<body>
+    <!-- Preloader
+    ================================================== --> 
+    <div class="preloader">
+        <div class="preloader-inner">
+            <div class="preloader-icon">
+                <span></span>
+                <span></span>
+            </div><!-- /preloader-icon -->
+        </div><!-- /preloader-inner -->
+    </div><!-- /preloader -->
+
+    <!-- Header
+    ================================================== --> 
+    <header class="site-header sticky-header">
+        <div class="container-fluid pd-0">
+            <div class="row no-gutters">
+                <div class="col-lg-7">
+                    <div class="header-left-block dark">  
+                        <!-- Site Branding -->
+                        <div class="site-branding">
+                            <a href="index.html"><img src="assets/images/logo.png" alt="Site Logo" /></a>
+                        </div><!--  /.site-branding -->
+                        
+                        <!-- Site Navigation -->
+                        <div class="site-navigation">
+                            <div class="hamburger-menus">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                            <nav class="navigation">
+                                <div class="overlaybg"></div><!--  /.overlaybg -->
+                                <!-- Main Menu -->
+                                <div class="menu-wrapper">
+                                    <div class="menu-content">
+                                        <ul class="mainmenu">         
+                                            <li>
+                                                <a href="index.html#service-block">Service</a>
+                                            </li>
+                                            <li>
+                                                <a href="index.html#about-block">About</a>
+                                            </li>                                    
+                                            <li>
+                                                <a href="index.html#portfolio-block">Works</a>
+                                            </li>
+                                            <li>
+                                                <a href="index.html#blog-block">Blog</a>
+                                            </li>
+                                            <li>
+                                                <a href="index.html#contact-block">Contact</a>
+                                            </li>
+                                        </ul> <!-- /.menu-list -->
+                                    </div> <!-- /.hours-content-->
+                                </div><!-- /.menu-wrapper --> 
+                            </nav>
+                        </div><!--  /.site-navigation -->
+                    </div><!--  /.header-left-block -->
+                </div><!--  /.col-lg-7 -->
+
+                <div class="col-lg-5">
+                    <div class="header-right-block bg-midnight-express dark">
+                        <div class="mail-block">
+                            <a href="mailto:example@domain.com"><i class="fas fa-envelope"></i><span>example@domain.com</span></a>
+                        </div><!--  /.mail-block -->
+                        <div class="call-block">
+                            <a href="tel:008969854756"><i class="fas fa-phone"></i><span>008969854756</span></a>
+                        </div><!--  /.call-block -->
+                        <div class="social-block">
+                            <ul>
+                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            </ul>
+                        </div><!--  /.social-block -->
+                    </div><!--  /.header-right-block -->
+                </div><!--  /.col-lg-5 -->
+            </div><!--  /.row -->
+        </div><!--  /.container-fluid -->
+    </header><!-- /.site-header -->
+
+    <!-- Blog Page Block
+    ================================================== -->
+    <section class="error-page-block dark bg-black-russian-3 pd-t-220 pd-b-120">
+        <div class="container">
+            <!-- Content Row -->
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h2 class="error-title">Ops Page Not Found!!!</h2><!--  /.error-title -->
+                    <h2 class="error-main">404</h2><!--  /.error-main -->
+                    <a href="index.html" class="btn btn-default go-home bg-midnight-express mrt-15">Go Home</a>
+                </div><!--  /.col-12 -->
+            </div><!--  /.row -->
+        </div><!--  /.container -->
+    </section><!--  /.blog-page-block -->
+
+    <!-- Footer
+    ================================================== -->
+    <footer class="site-footer bg-midnight-express pd-t-75 pd-b-75">
+        <div class="container text-center">
+            <!-- Scroll Top -->
+            <div class="row">
+                <div class="col-12">
+                    <a href="#top" class="back-to-top dark">
+                        <span class="text">Back <br>To Top</span>
+                        <i class="fas fa-angle-up"></i>
+                    </a>
+                </div><!--  /.col-12 -->
+            </div><!--  /.row -->
+
+            <!-- Social Link -->
+            <div class="row">
+                <div class="col-12">
+                    <ul class="footer-social dark mrt-30 mrb-30">
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Pintarest</a></li>
+                        <li><a href="#">Behance</a></li>
+                        <li><a href="#">Dribble</a></li>
+                    </ul><!--  /.footer-social -->
+                </div><!--  /.col-12 -->
+            </div><!--  /.row -->
+
+            <!-- Copy Right -->
+            <div class="row">
+                <div class="col-12">
+                    <p class="copyright-text color-gainsboro">Htmlguru <i class="fas fa-heart"></i> 2018. All rights reserved</p>
+                </div><!--  /.col-12 -->
+            </div><!--  /.row -->
+        </div><!--  /.container -->
+    </footer><!--  /.site-footer -->
+
+    <!-- All The JS Files
+    ================================================== --> 
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/jquery.validate.min.js"></script>
+    <script src="assets/js/jquery-validate.bootstrap-tooltip.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/jquery.fitvids.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/simple-scrollbar.min.js"></script>
+    <script src="assets/js/scrolla.jquery.min.js"></script>
+    <script src="assets/js/odometer.min.js"></script>
+    <script src="assets/js/isInViewport.jquery.js"></script>
+    <script src="assets/js/main.js"></script><!-- main-js -->
+</body>
+</html>
